0ae780809913c32a69f9c40e1bd4c4f3fe309a71
Game.Play() <gamedotplay0@gmail.com>
2023-07-24 14:39:38 -0400

    Delete PhysicalDeviceType.hpp
    This type is no longer used.

--- app/src/main/cpp/PhysicalDeviceType.hpp
+++
@@ -1,7 +0,0 @@
-#include <vulkan/vulkan_core.h>
-#include <string>
-
-struct PhysicalDeviceType
-{
-    static inline std::string PhysicalDeviceTypeNames[5] = {"Other", "Integrated", "Discrete", "Virtual", "CPU"};
-};

62babd8a83adc842fa07d3c20e8ccfd6269d0689
Game.Play() <gamedotplay0@gmail.com>
2023-07-27 22:12:44 -0400

    Create PhysicalDeviceFeatures.java

---
+++ app/src/main/java/com/example/vulkaninfoapp/PhysicalDeviceFeatures.java
@@ -0,0 +1,117 @@
+package com.example.vulkaninfoapp;
+
+public class PhysicalDeviceFeatures {
+    public boolean robustBufferAccess;
+    public boolean fullDrawIndexUint32;
+    public boolean imageCubeArray;
+    public boolean independentBlend;
+    public boolean geometryShader;
+    public boolean tessellationShader;
+    public boolean sampleRateShading;
+    public boolean dualSrcBlend;
+    public boolean logicOp;
+    public boolean multiDrawIndirect;
+    public boolean drawIndirectFirstInstance;
+    public boolean depthClamp;
+    public boolean depthBiasClamp;
+    public boolean fillModeNonSolid;
+    public boolean depthBounds;
+    public boolean wideLines;
+    public boolean largePoints;
+    public boolean alphaToOne;
+    public boolean multiViewport;
+    public boolean samplerAnisotropy;
+    public boolean textureCompressionETC2;
+    public boolean textureCompressionASTC_LDR;
+    public boolean textureCompressionBC;
+    public boolean occlusionQueryPrecise;
+    public boolean pipelineStatisticsQuery;
+    public boolean vertexPipelineStoresAndAtomics;
+    public boolean fragmentStoresAndAtomics;
+    public boolean shaderTessellationAndGeometryPointSize;
+    public boolean shaderImageGatherExtended;
+    public boolean shaderStorageImageExtendedFormats;
+    public boolean shaderStorageImageMultisample;
+    public boolean shaderStorageImageReadWithoutFormat;
+    public boolean shaderStorageImageWriteWithoutFormat;
+    public boolean shaderUniformBufferArrayDynamicIndexing;
+    public boolean shaderSampledImageArrayDynamicIndexing;
+    public boolean shaderStorageBufferArrayDynamicIndexing;
+    public boolean shaderStorageImageArrayDynamicIndexing;
+    public boolean shaderClipDistance;
+    public boolean shaderCullDistance;
+    public boolean shaderFloat64;
+    public boolean shaderInt64;
+    public boolean shaderInt16;
+    public boolean shaderResourceResidency;
+    public boolean shaderResourceMinLod;
+    public boolean sparseBinding;
+    public boolean sparseResidencyBuffer;
+    public boolean sparseResidencyImage2D;
+    public boolean sparseResidencyImage3D;
+    public boolean sparseResidency2Samples;
+    public boolean sparseResidency4Samples;
+    public boolean sparseResidency8Samples;
+    public boolean sparseResidency16Samples;
+    public boolean sparseResidencyAliased;
+    public boolean variableMultisampleRate;
+    public boolean inheritedQueries;
+
+    public static final String[] PhysicalDeviceFeatureNames = {
+            "Robust buffer access",
+            "Full draw index uint 32",
+            "Image cube array",
+            "Independent blend",
+            "Geometry shader",
+            "Tessellation shader",
+            "Sample rate shading",
+            "Dual src blend",
+            "Logic op",
+            "Multi draw indirect",
+            "Draw indirect first instance",
+            "Depth clamp",
+            "Depth bias clamp",
+            "Fill mode non-solid",
+            "Depth bounds",
+            "Wide lines",
+            "Large points",
+            "Alpha to one",
+            "Multi-viewport",
+            "Sampler anisotropy",
+            "Texture compression ETC2",
+            "Texture compression ASTC-LDR",
+            "Texture compression BC",
+            "Occlusion query precise",
+            "Pipeline statistics query",
+            "Vertex pipeline stores & atomics",
+            "Fragment stores & atomics",
+            "Shader tessellation & geometry point size",
+            "Shader image gather extended",
+            "Shader storage image extended formats",
+            "Shader storage image multi-sample",
+            "Shader storage image read without format",
+            "Shader storage image write without format",
+            "Shader uniform buffer array dynamic indexing",
+            "Shader sampled image array dynamic indexing",
+            "Shader storage buffer array dynamic indexing",
+            "Shader storage image array dynamic indexing",
+            "Shader clip distance",
+            "Shader cull distance",
+            "Shader float 64",
+            "Shader int 64",
+            "Shader int 16",
+            "Shader resource residency",
+            "Shader resource min lod",
+            "Sparse binding",
+            "Sparse residency buffer",
+            "Sparse residency image 2D",
+            "Sparse residency image 3D",
+            "Sparse residency 2 samples",
+            "Sparse residency 4 samples",
+            "Sparse residency 8 samples",
+            "Sparse residency 16 samples",
+            "Sparse residency aliased",
+            "Variable multi-sample rate",
+            "Inherited queries"
+    };
+}

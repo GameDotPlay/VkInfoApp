b1ac8056efb8b0fef380182934541fc04f4ef97e
Game.Play() <gamedotplay0@gmail.com>
2023-07-28 22:16:05 -0400

    Update VkInfo.java

--- app/src/main/java/com/example/vulkaninfoapp/VkInfo.java
+++ app/src/main/java/com/example/vulkaninfoapp/VkInfo.java
@@ -6,4 +6,6 @@ public class VkInfo {
     public PhysicalDeviceProperties physicalDeviceProperties;
 
     public PhysicalDeviceFeatures physicalDeviceFeatures;
+
+    public PhysicalDeviceMemoryProperties physicalDeviceMemoryProperties;
 }

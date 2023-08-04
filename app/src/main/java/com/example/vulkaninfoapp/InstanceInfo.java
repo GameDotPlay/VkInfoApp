8438f1491a7df6dc3561356aef74c97979ac985c
Game.Play() <gamedotplay0@gmail.com>
2023-08-03 21:03:51 -0400

    Update InstanceInfo.java
    Added new fields for extension properties.

--- app/src/main/java/com/example/vulkaninfoapp/InstanceInfo.java
+++ app/src/main/java/com/example/vulkaninfoapp/InstanceInfo.java
@@ -4,11 +4,16 @@ public class InstanceInfo {
 
     public String appName;
     public String engineName;
-    public int numDevices;
+    public long numExtensions;
+    public ExtensionProperties[] availableExtensions;
+
+    public long numDevices;
 
     public static final String[] InstancePropertyNames = {
             "Application name",
             "Engine name",
+            "Number of Available Extensions",
+            "Available Extensions",
             "Number of devices"
     };
 }

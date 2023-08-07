d3b0be91b8cdff81100e14d27e97717ca025103d
Game.Play() <gamedotplay0@gmail.com>
2023-08-07 08:22:24 -0400

    Renamed class.

--- app/src/main/java/com/example/vulkaninfoapp/ExpandableLeafLevelAdapter.java
+++ app/src/main/java/com/example/vulkaninfoapp/ExpandableLeafLevelAdapter.java
@@ -12,36 +12,36 @@ import android.widget.TextView;
 import java.util.List;
 import java.util.Map;
 
-public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+public class ExpandableLeafLevelAdapter extends BaseExpandableListAdapter {
 
     private Context context;
-    private Map<String, List<Pair<String, String>>> mobileCollection;
-    private List<String> groupList;
+    private Map<String, List<Pair<String, String>>> leafCollection;
+    private List<String> propertyGroupList;
 
-    public MyExpandableListAdapter(Context context, List<String> groupList, Map<String, List<Pair<String, String>>> mobileCollection) {
+    public ExpandableLeafLevelAdapter(Context context, List<String> propertyGroupList, Map<String, List<Pair<String, String>>> leafCollection) {
         this.context = context;
-        this.mobileCollection = mobileCollection;
-        this.groupList = groupList;
+        this.leafCollection = leafCollection;
+        this.propertyGroupList = propertyGroupList;
     }
 
     @Override
     public int getGroupCount() {
-        return mobileCollection.size();
+        return leafCollection.size();
     }
 
     @Override
     public int getChildrenCount(int i) {
-        return mobileCollection.get(groupList.get(i)).size();
+        return leafCollection.get(propertyGroupList.get(i)).size();
     }
 
     @Override
     public Object getGroup(int i) {
-        return groupList.get(i);
+        return propertyGroupList.get(i);
     }
 
     @Override
     public Object getChild(int i, int i1) {
-        return mobileCollection.get(groupList.get(i)).get(i1);
+        return leafCollection.get(propertyGroupList.get(i)).get(i1);
     }
 
     @Override
@@ -61,14 +61,14 @@ public class MyExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
-        String mobileName = getGroup(i).toString();
+        String groupName = getGroup(i).toString();
         if (view == null) {
             LayoutInflater inflator = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflator.inflate(R.layout.group_item, null);
         }
         TextView item = view.findViewById(R.id.propertyCategory);
         item.setTypeface(null, Typeface.BOLD);
-        item.setText(mobileName);
+        item.setText(groupName);
         return view;
     }
 
